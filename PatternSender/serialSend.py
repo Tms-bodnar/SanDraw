9a0238c0e9878ee5ed27991505fae6d8b5746eac
Tms-bodnar <tms.bodnar@gmail.com>
2020-12-06 16:45:43 +0100

    refactored for understandable code

--- PatternSender/serialSend.py
+++ PatternSender/serialSend.py
@@ -2,124 +2,131 @@ import math
 import os
 import serial
 
-fileList = []
-start = "S"
-end = "E"
-last = False
-MICRO_STEP_CORR = 0.1125
-max_radius = 0
-prev_tht: float
-prev_z: float
-prev_z_for_arm1: float
-step_div_remainder_arm2 = 0
-step_div_remainder_arm1 = 0
+waiting_for_arduino = True
 
-first_line = False
+# variables file ops
+fileList = []
+start_code = "S"
+end_code = "E"
+line_is_first = False
 path = '/home/pi/SanDraw/PatternSender/patternDir'
-res_file = open(path + '/res.csv', 'a')
+steps_result_file = open(path + '/res.csv', 'a')
+
+# variables step calculation
+MICRO_STEP_CORR = 0.1125
+ARM_LENGTH = 0.5
+GEAR_RATIO = 1.333333
+POSITIVE = 1
+NEGATIVE = -1
+theta_previous: float
+rho_previous: float
+rho_previous_for_arm1: float
+arm2_step_remainder = 0
+arm1_step_remainder = 0
+
+# read *.tht files to array
 for r, d, f in os.walk(path):
     for file in f:
         if '.thr' in file:
             fileList.append(file)
+
+# start serial
 ser = serial.Serial('/dev/ttyACM0', '9600')
 ser.flushInput()
 
 
+# calculate steps from theta-rho pairs
 def parse_degrees(line):
-    global first_line
-    global prev_tht
-    global prev_z
-    global prev_z_for_arm1
-    global step_div_remainder_arm2
-    global step_div_remainder_arm1
-    arm2_multiplier = 1;
+    global ARM_LENGTH
+    global line_is_first
+    global theta_previous
+    global rho_previous
+    global rho_previous_for_arm1
+    global arm1_step_remainder
+    global arm2_step_remainder
+
+    arm2_direction_modifier = POSITIVE  # variable for arm2 direction
     raws = line.split(" ")
-    z: float
-    z_for_arm1: float
-    tht: float
-    prev_tht_deg: float
-    arm_length = 0.5
-    tht = float(raws[0])
-    z = float(raws[1])
-    if first_line:
-        prev_z = z
-        prev_tht = tht
-        prev_z_for_arm1 = z
-    arm1_step: float
-    arm1_deg: float
-    z_for_arm1 = z
-    if tht < 0:
-        tht += math.pi
-        z_for_arm1 *= -1
-        arm2_multiplier *= -1
-    tht_deg = round(math.degrees(tht), 6)
-    prev_tht_deg = round(math.degrees(prev_tht), 6)
-    arm1_deg = round(tht_deg + math.degrees(math.acos(z_for_arm1)), 6)
-    prev_arm1_deg = round(prev_tht_deg + math.degrees(math.acos(prev_z_for_arm1)), 6)
-    arm1_step = ((prev_arm1_deg - arm1_deg) / MICRO_STEP_CORR) * 1.333333
-    step_div_remainder_arm1 += arm1_step % 1
-    if abs(step_div_remainder_arm1) > 1:
-        arm1_step += step_div_remainder_arm1
-        step_div_remainder_arm1 = arm1_step % 1
-        
-    prev_arm2_deg = round(math.degrees(math.acos((arm_length - prev_z * prev_z) / arm_length)), 6)       
-    arm2_deg = round(math.degrees(math.acos((arm_length - z * z) / arm_length)), 6) 
-    arm2_step = (prev_arm2_deg - arm2_deg) / MICRO_STEP_CORR * arm2_multiplier
-    step_div_remainder_arm2 += arm2_step % 1
-    if abs(step_div_remainder_arm2) > 1:
-        arm2_step += step_div_remainder_arm2
-        step_div_remainder_arm2 = arm2_step % 1
+    theta = float(raws[0])
+    rho = float(raws[1])
+
+    if line_is_first:
+        theta_previous = theta
+        rho_previous = rho
+        rho_previous_for_arm1 = rho
+
+    rho_for_arm1_calc = rho
+    if theta < 0:
+        theta += math.pi
+        rho_for_arm1_calc *= -1
+        arm2_direction_modifier = NEGATIVE
+
+    theta_degrees = round(math.degrees(theta), 6)
+    theta_degrees_previous = round(math.degrees(theta_previous), 6)
+
+    arm1_degrees = round(theta_degrees + math.degrees(math.acos(rho_for_arm1_calc)), 6)
+    arm1_degrees_previous = round(theta_degrees_previous + math.degrees(math.acos(rho_previous_for_arm1)), 6)
+    arm1_step = ((arm1_degrees_previous - arm1_degrees) / MICRO_STEP_CORR) * GEAR_RATIO
+
+    arm1_step_remainder += arm1_step % 1
+    if abs(arm1_step_remainder) > 1:
+        arm1_step += arm1_step_remainder
+        arm1_step_remainder = arm1_step % 1
+
+    arm2_degrees = round(math.degrees(math.acos((ARM_LENGTH - rho * rho) / ARM_LENGTH)), 6)
+    arm2_degrees_previous = round(math.degrees(math.acos((ARM_LENGTH - rho_previous * rho_previous) / ARM_LENGTH)), 6)
+    arm2_step = (arm2_degrees_previous - arm2_degrees) / MICRO_STEP_CORR * arm2_direction_modifier
+
+    arm2_step_remainder += arm2_step % 1
+    if abs(arm2_step_remainder) > 1:
+        arm2_step += arm2_step_remainder
+        arm2_step_remainder = arm2_step % 1
      
     coordinates = "<" + str(arm1_step).split(".")[0].zfill(5) + "T" + str(arm2_step).split(".")[0].zfill(5) + "R>"
-    if not first_line:
-        res_file.write(str(arm1_step).split(".")[0] + ',' + str(arm2_step).split(".")[0]  + '\n')
+
+    if line_is_first:
+        steps_result_file.write('x,y' + '\n')
+        line_is_first = False
+    else:
+        steps_result_file.write(str(arm1_step).split(".")[0] + ',' + str(arm2_step).split(".")[0] + '\n')
         ser.write(coordinates.encode('utf-8'))
         print(coordinates)
         check_response()
-    else:
-        res_file.write('x,y' + '\n')
-    prev_tht = tht
-    prev_z = z
-    prev_z_for_arm1 = z_for_arm1
-    first_line = False
+
+    theta_previous = theta
+    rho_previous = rho
+    rho_previous_for_arm1 = rho_for_arm1_calc
 
 
 def check_response():
     wait = True
     while wait:
-        check_resp = ser.readline()
-        if "Done" in check_resp.decode('utf-8'):
+        response = ser.readline()
+        if "Done" in response.decode('utf-8'):
             wait = False
         
 
 def send_coordinates():
-    global max_radius
     for oneFile in fileList:
         with open(path + "/" + oneFile) as openedFile:
-            global first_line
-            global last
-            first_line = True
+            global line_is_first
+            line_is_first = True
             lines = openedFile.readlines()
-            last = lines[-1]
+            last_line = lines[-1]
             for line in lines:
                 if not line:
-                    last = True
                     break
-                if line.find('Max Radius: ') > 0:
-                    max_radius = float(line[18:])
                 if not line.startswith('#') and not line.startswith('\n'):
                     parse_degrees(line)
-                    
-                if line is last:
-                    res_file.close()
-                    ser.write(end.encode('utf-8'))
+                if line is last_line:
+                    steps_result_file.close()
+                    ser.write(end_code.encode('utf-8'))
                     break
 
 
-x = 0
-while x < 1:
-    ser.write(start.encode('utf-8'))
+while waiting_for_arduino:
+    ser.write(start_code.encode('utf-8'))
     resp = ser.readline()
     if "Waiting" in resp.decode('utf-8'):
         send_coordinates()
-        x += 1
+        waiting_for_arduino = False
